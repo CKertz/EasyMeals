ba058b38d2617e7d0f3b9029bed2ed5f72681239
Cooper <tbd@TBDs-MBP.att.net>
2017-07-26 22:18:07 -0500

    New Main Menu View underway:

--- EasyMeals/FirstViewController.swift
+++ EasyMeals/FirstViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UITableViewController {
 
+    
+   /* let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor.blue
+        button.setTitle("Register", for: .normal)
+        button.layer.cornerRadius = 6
+        button.layer.masksToBounds = true
+        //button.titleLabel?.font = UIFont.init(name: "Futura Medium" , size: 16)
+        return button
+    }()*/
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector (handleLogout))
         // Do any additional setup after loading the view, typically from a nib.
+      /*  let firstViewController = FirstViewController()
+        self.present(firstViewController,animated: true, completion: nil)
+        
+        //..present(firstViewController,animated: true, completion: nil)
+        let mainMenuContainerView = UIView()
+        
+        view.addSubview(mainMenuContainerView)
+        view.addSubview(loginButton)
+        
+        setupMainMenu()*/
     }
+    func handleLogout(){
+        let loginController = SecondViewController()
+        present(loginController, animated: true, completion: nil)
+    }
+   /* func setupMainMenu(){
 
+        loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        loginButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -25).isActive = true
+        loginButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }*/
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- EasyMeals/SecondViewController.swift
+++ EasyMeals/SecondViewController.swift
@@ -9,17 +9,82 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+   
+    let continueAsGuestLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Continue as guest"
+        label.font=UIFont.init(name: "Futura Medium", size: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    let signInButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor.white
+        button.setTitle("Sign In", for: .normal)
+        button.layer.cornerRadius = 6
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = UIFont.init(name: "Futura Medium", size: 16)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+        
+    }()
+    let registerButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor.white
+        button.setTitle("Register", for: .normal)
+        button.layer.cornerRadius = 6
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = UIFont.init(name: "Futura Medium" , size: 16)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    let loginContainerView: UIView = {
+        
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        view.backgroundColor = UIColor.blue
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        view.addSubview(loginContainerView)
+        view.addSubview(registerButton)
+        view.addSubview(signInButton)
+        view.addSubview(continueAsGuestLabel)
+        
+        //setupLoginContainer()
+        setupRegisterButton()
 
+    }
+    func setupLoginContainer(){
+        loginContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        loginContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -25).isActive = true
+        loginContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+    }
+    func setupRegisterButton(){
+        registerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        registerButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        registerButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -25).isActive = true
+        registerButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        signInButton.leftAnchor.constraint(equalTo: registerButton.leftAnchor).isActive = true
+        signInButton.topAnchor.constraint(equalTo: registerButton.bottomAnchor, constant: 10).isActive = true
+        signInButton.widthAnchor.constraint(equalTo: registerButton.widthAnchor).isActive = true
+        signInButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        //continueAsGuestLabel.center = CGPoint(x: <#T##CGFloat#>, y: <#T##CGFloat#>)
+        //continueAsGuestLabel.leftAnchor.constraint(equalTo: registerButton.leftAnchor).isActive = true
+        continueAsGuestLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        //continueAsGuestLabel.centerXAnchor.constraint(equalTo: registerButton.centerXAnchor).isActive = true
+        continueAsGuestLabel.topAnchor.constraint(equalTo: signInButton.bottomAnchor, constant: 10).isActive = true
+        continueAsGuestLabel.widthAnchor.constraint(equalTo: registerButton.widthAnchor).isActive = true
+        continueAsGuestLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+    }
 
 }
 
